4cbe577e4f1e8318e27fb84bd2e1677d50eee0b6
navaroji <63150508+navaroji@users.noreply.github.com>
2021-06-08 12:11:14 +0530

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contacts.php",
+                url: "contact.php",
                 type: "POST",
                 data: {
                     name: name,
